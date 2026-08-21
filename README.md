dee36e37a27168d62f3596f83b1c2a888b23ae21
Dimantha Kaveen <vpdkkaveen@gmail.com>
2026-08-21 18:11:01 +0530

    feat: integrate GCP Cloud Storage and finalize configurations

---
+++ README.md
@@ -0,0 +1,5 @@
+# Capstone Project
+### Student Information
+- **Student Name**: Dimantha Kaveen
+- **Student Number**: 241722029
+- **GCP Project ID**: project-46ec948e-0d3d-4923-927
---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [
+    {
+      name: "cloud-sql-auth-proxy",
+      script: " ./cloud-sql-proxy hdse-capstone:asia-southeast1:mysql-vm hdse-capstone:asia-southeast1:postgres-vm --private-ip",
+      log_file: "./logs/cloud-sql-proxy.log"
+
+    },
+    {
+      name: "student-service",
+      script: "java -jar student-service/target/Student-Service-1.0.0.jar",
+      log_file: "./logs/student-service.log",
+      instance: 2
+    },
+
+
+    {
+      name: "program-service",
+      script: "java -jar program-service/target/Program-Service-1.0.0.jar ",
+      log_file: "./logs/program-service.log",
+      instance: 2
+    },
+    {
+      name: "enrollment-service",
+      script: "java -jar enrollment-service/target/Enrollment-Service-1.0.0.jar ",
+      log_file: "./logs/enrollment-service.log",
+      instance: 2
+    }
+  ]
+}
