8e89fe90746ee04132f22a55daf43af7c6e3d7ca
Dimantha Kaveen <vpdkkaveen@gmail.com>
2026-08-25 18:33:46 +0530

    fix: update PM2 ecosystem to run java processes with prod profile

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,22 @@
-module.exports = {
+﻿module.exports = {
   apps: [
     {
       name: "student-service",
-      script: "java.exe",
-      args: "-jar student-service/target/Student-Service-1.0.0.jar",
-      log_file: "./logs/student-service.log",
-      env: { SPRING_PROFILES_ACTIVE: 'dev', SPRING_CLOUD_CONFIG_URI: 'http://localhost:9000' }
+      script: "java",
+      args: "-jar student-service/target/Student-Service-1.0.0.jar --spring.profiles.active=prod",
+      log_file: "./logs/student-service.log"
     },
     {
       name: "program-service",
-      script: "java.exe",
-      args: "-jar program-service/target/Program-Service-1.0.0.jar",
-      log_file: "./logs/program-service.log",
-      env: { SPRING_PROFILES_ACTIVE: 'dev', SPRING_CLOUD_CONFIG_URI: 'http://localhost:9000' }
+      script: "java",
+      args: "-jar program-service/target/Program-Service-1.0.0.jar --spring.profiles.active=prod",
+      log_file: "./logs/program-service.log"
     },
     {
       name: "enrollment-service",
-      script: "java.exe",
-      args: "-jar enrollment-service/target/Enrollment-Service-1.0.0.jar",
-      log_file: "./logs/enrollment-service.log",
-      env: { SPRING_PROFILES_ACTIVE: 'dev', SPRING_CLOUD_CONFIG_URI: 'http://localhost:9000' }
+      script: "java",
+      args: "-jar enrollment-service/target/Enrollment-Service-1.0.0.jar --spring.profiles.active=prod",
+      log_file: "./logs/enrollment-service.log"
     }
   ]
-}
+};
