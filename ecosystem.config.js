16ed4b629fe1ef88fe09288c41e41da9c41a4e63
Dimantha Kaveen <vpdkkaveen@gmail.com>
2026-08-25 19:05:52 +0530

    fix: switch to dev profile for correct routing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,20 +3,21 @@
     {
       name: "student-service",
       script: "java",
-      args: "-jar student-service/target/Student-Service-1.0.0.jar --spring.profiles.active=prod",
+      args: "-jar student-service/target/Student-Service-1.0.0.jar --spring.profiles.active=dev",
       log_file: "./logs/student-service.log"
     },
     {
       name: "program-service",
       script: "java",
-      args: "-jar program-service/target/Program-Service-1.0.0.jar --spring.profiles.active=prod",
+      args: "-jar program-service/target/Program-Service-1.0.0.jar --spring.profiles.active=dev",
       log_file: "./logs/program-service.log"
     },
     {
       name: "enrollment-service",
       script: "java",
-      args: "-jar enrollment-service/target/Enrollment-Service-1.0.0.jar --spring.profiles.active=prod",
+      args: "-jar enrollment-service/target/Enrollment-Service-1.0.0.jar --spring.profiles.active=dev",
       log_file: "./logs/enrollment-service.log"
     }
   ]
 };
+
