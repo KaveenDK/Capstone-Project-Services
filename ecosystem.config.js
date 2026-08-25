2c22efb6242b320c53766c2ab4f69239a63e10aa
Dimantha Kaveen <vpdkkaveen@gmail.com>
2026-08-25 19:24:33 +0530

    fix: revert to prod profile for correct database bindings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,21 +3,22 @@
     {
       name: "student-service",
       script: "java",
-      args: "-jar student-service/target/Student-Service-1.0.0.jar --spring.profiles.active=dev",
+      args: "-jar student-service/target/Student-Service-1.0.0.jar --spring.profiles.active=prod",
       log_file: "./logs/student-service.log"
     },
     {
       name: "program-service",
       script: "java",
-      args: "-jar program-service/target/Program-Service-1.0.0.jar --spring.profiles.active=dev",
+      args: "-jar program-service/target/Program-Service-1.0.0.jar --spring.profiles.active=prod",
       log_file: "./logs/program-service.log"
     },
     {
       name: "enrollment-service",
       script: "java",
-      args: "-jar enrollment-service/target/Enrollment-Service-1.0.0.jar --spring.profiles.active=dev",
+      args: "-jar enrollment-service/target/Enrollment-Service-1.0.0.jar --spring.profiles.active=prod",
       log_file: "./logs/enrollment-service.log"
     }
   ]
 };
+
 
